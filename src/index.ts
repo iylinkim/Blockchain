57bf2e3f143e9d394604b86d7c75e9ef38f1cc58
iylinkim <kiylin94@naver.com>
2021-02-23 11:36:25 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-//Typescript에서만 작동함
-interface Human{
-  name:string,
-  age:number,
-  gender:string
+class Human{
+  //public, private 은 Typescript에만 있음
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name:string, age:number, gender:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Iylin",
-  age:22,
-  gender: "female",
-}
+const me = new Human("Iylin", 18,"female");
 
 const sayHi = (person:Human):string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
-console.log(sayHi(person));
+console.log(sayHi(me));
 
 export {};
