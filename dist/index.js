1b26c6cd69539cf54fe73440baad2b93093b42b4
iylinkim <kiylin94@naver.com>
2021-02-23 12:05:39 +0900

    Creating a Block - part2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,9 +10,13 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+//static으로 method를 생성한다면, new Block을 생성하지 않아도 사용할 수 있음
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "202020", "", "hello", 123456);
 let blockchain = [genesisBlock];
 // Block만 blockchain에 추가하도록 체크함
 // blockchain.push("stuff") Block이 아니기 때문에 추가할 수 없음
-console.log(blockchain);
+const getBlockChain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  //static으로 method를 생성한다면, new Block을 생성하지 않아도 사용할 수 있음
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -21,9 +33,12 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "202020", "", "hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 // Block만 blockchain에 추가하도록 체크함
 // blockchain.push("stuff") Block이 아니기 때문에 추가할 수 없음
-console.log(blockchain)
+
+const getBlockChain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
